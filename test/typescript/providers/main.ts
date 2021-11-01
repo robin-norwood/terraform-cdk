5c73c45694cf5754194462ef289c66cd1a371ddf
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-11-01 16:18:17 +0100

    test(cli): test generated providers

--- test/typescript/providers/main.ts
+++ test/typescript/providers/main.ts
@@ -1,5 +1,5 @@
 import { Construct } from "constructs";
-import { App, TerraformStack, Testing } from "cdktf";
+import { App, TerraformStack, TerraformVariable, Testing } from "cdktf";
 import * as NullProvider from "./.gen/providers/null";
 import * as Aws from "./.gen/providers/aws";
 import * as Azure from "./.gen/providers/azurerm";
@@ -11,7 +11,7 @@ import * as Vault from "./.gen/providers/vault";
 import * as Consul from "./.gen/providers/consul";
 import * as External from "./.gen/providers/external";
 
-export class HelloTerra extends TerraformStack {
+export class UsingAllProviders extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
     new NullProvider.NullProvider(this, "null", {});
@@ -30,6 +30,159 @@ export class HelloTerra extends TerraformStack {
   }
 }
 
+export class NamespacedProviders extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    new Aws.AwsProvider(this, "aws", {
+      region: "us-east-1",
+    });
+
+    const ami = new Aws.EC2.DataAwsAmi(this, "ami", {
+      mostRecent: true,
+      owners: ["amazon"],
+    });
+
+    new Aws.EC2.Instance(this, "instance", {
+      ami: ami.id,
+      availabilityZone: "us-east-1a",
+      instanceType: "t2.micro",
+    });
+
+    const userId = new Aws.DataSources.DataAwsCallerIdentity(
+      this,
+      "callerIdentity",
+      {}
+    );
+    new Aws.LambdaFunction.LambdaFunction(this, "lambdaFn", {
+      handler: "index.handler",
+      runtime: "nodejs12.x",
+      timeout: 10,
+      functionName: userId.accountId,
+      role: new Aws.IAM.IamRole(this, "role", {
+        assumeRolePolicy: "assumeRolePolicy",
+      }).arn,
+    });
+  }
+}
+
+// TODO: add provider "references"
+export class References extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // simple references
+    new Nomad.NomadProvider(this, "nomad", { address: "http://127.0.0.1" });
+    const job = new Nomad.JobA(this, "jobA", {
+      jobspec: "./job/spec.hcl",
+    });
+
+    new Nomad.JobA(this, "jobB", {
+      jobspec: job.jobspec,
+    });
+
+    // single-item references
+    new Kubernetes.KubernetesProvider(this, "k8s", {});
+    const namespace = new Kubernetes.NamespaceA(this, "namespace", {
+      metadata: { name: "my-namespace" },
+    });
+    new Kubernetes.Deployment(this, "deployment", {
+      metadata: {
+        name: "my-deployment",
+        namespace: namespace.metadata.name,
+      },
+      spec: {
+        replicas: "1",
+        selector: {
+          matchLabels: {
+            app: "my-deployment",
+          },
+        },
+        template: {
+          metadata: {
+            labels: {
+              app: "my-deployment",
+            },
+          },
+          spec: {
+            container: [
+              {
+                name: "my-deployment",
+                image: "nginx",
+              },
+            ],
+          },
+        },
+      },
+    });
+  }
+}
+
+export class Mutation extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const namespace = new TerraformVariable(this, "namespace", {
+      type: "string",
+      default: "default",
+    }).stringValue;
+
+    new Kubernetes.KubernetesProvider(this, "k8s", {});
+    const deployment = new Kubernetes.Deployment(this, "deployment", {
+      metadata: { name: "my-deployment" },
+      spec: {
+        replicas: "1",
+        selector: {
+          matchLabels: {
+            app: "my-deployment",
+          },
+        },
+        template: {
+          metadata: {
+            labels: {
+              app: "my-deployment",
+            },
+          },
+          spec: {
+            container: [
+              {
+                name: "my-deployment",
+                image: "nginx",
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    // direct primitive mutation
+    deployment.spec.replicas = "2";
+
+    // direct reference mutation
+    deployment.metadata.namespace = namespace;
+
+    // object mutation (not yet supported
+    // deployment.spec.selector = {
+    //   matchLabels: {
+    //     app: "my-other-deployment",
+    //   },
+    // }
+
+    // list mutation (not yet supported)
+    // const containerImage = new TerraformVariable(this, "containerImage", {
+    //   type: "string",
+    //   default: "nginix",
+    // }).stringValue;
+
+    // deployment.spec.template.spec.container?.push({
+    //   name: "my-other-container",
+    //   image: containerImage,
+    // });
+  }
+}
+
 const app = Testing.stubVersion(new App({}));
-new HelloTerra(app, "hello-deploy");
+new UsingAllProviders(app, "using-all-providers");
+new NamespacedProviders(app, "namespaces");
+new References(app, "references");
+new Mutation(app, "mutation");
 app.synth();
--- test/typescript/providers/test.ts
+++ test/typescript/providers/test.ts
@@ -9,6 +9,29 @@ describe("full integration test", () => {
   });
 
   test("build providers", () => {
-    expect(driver.deploy()).toMatchSnapshot();
+    expect(driver.deploy("using-all-providers")).toMatchSnapshot();
+  });
+
+  describe("provider functionality", () => {
+    beforeAll(async () => {
+      await driver.synth();
+    });
+
+    test("namespaces", () => {
+      const stack = driver.synthesizedStack("namespaces");
+      expect(stack).toMatchSnapshot();
+    });
+
+    test("references", () => {
+      const stack = driver.synthesizedStack("references");
+      // TODO: write a proper test matcher
+      expect(stack).toMatchSnapshot();
+    });
+
+    test("mutation", () => {
+      const stack = driver.synthesizedStack("mutation");
+      // TODO: write a proper test matcher
+      expect(stack).toMatchSnapshot();
+    });
   });
 });
